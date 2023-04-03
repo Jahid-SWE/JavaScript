64a7c4866d20c8df3f7e1ee040ffa645841a0915
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2023-04-03 19:18:55 +0600

    Variables let, var, const

---
+++ Variables/script.js
@@ -0,0 +1,15 @@
+function demo(){
+    for(var i=1; i<5; i++ ){
+        console.log('I am Jahid Hassan');
+    }
+    console.log(i);
+}
+demo();
+
+var a=20;
+var a=29;
+console.log(a);
+
+// let a=10;
+// let a=20;
+// console.log(a);
